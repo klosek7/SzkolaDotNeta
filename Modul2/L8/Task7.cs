cd0dc58d4207f3f73b7d295af9dd4387a75525b9
klosek7 <lukaszklosek7@gmail.com>
2024-03-04 16:03:53 +0100

    Task 7 module 2 lesson 8

--- Modul2/L8/Task7.cs
+++ Modul2/L8/Task7.cs
@@ -1,18 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SzkolaDotNeta.Modul2.L8
+﻿namespace SzkolaDotNeta.Modul2.L8
 {
     class Task2_8_7
     {
         public static void Task7()
         {
-            
-            
+            /* Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej 
+            przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
+                *    
+               *** 
+              *****
+             *******
+            *********
+             *******
+              *****
+              ***
+               * 
+                        */
+            Console.WriteLine("Wprowadź liczbę: ");
+            int.TryParse(Console.ReadLine(), out int number);
+
+            for (int i = 1; i <= number; i++) 
+            {
+                for (int j = 1; j <= number - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.WriteLine("");
+            }
+            for (int i = number - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= number - i; j++)
+                    Console.Write(" ");  
+
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");  
+
+                Console.Write("\n"); 
+            }
+
+
         }
     }
 }
-    
+
