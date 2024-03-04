f31945a1cdd42465904dd790343da24e563a29aa
klosek7 <lukaszklosek7@gmail.com>
2024-03-04 14:50:27 +0100

    Task 6 module 2 lesson 8

--- Modul2/L8/Task6.cs
+++ Modul2/L8/Task6.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SzkolaDotNeta.Modul2.L8
+﻿namespace SzkolaDotNeta.Modul2.L8
 {
     class Task2_8_6
     {
         public static void Task6()
         {
-            
-            
+            /* Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+             1 + ½ +1 / 3 + ¼ itd.*/
+
+            double result = 0;
+
+            for (double i = 1; i <= 20; i++)
+            {
+                result += 1 / i;
+            }
+            Console.WriteLine(result);
         }
     }
 }
-    
+
