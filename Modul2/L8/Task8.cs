6270d5f5e10f9a36321efdd23911eec83a98451d
klosek7 <lukaszklosek7@gmail.com>
2024-03-05 08:57:17 +0100

    Task 8 module 2 lesson 8

--- Modul2/L8/Task8.cs
+++ Modul2/L8/Task8.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SzkolaDotNeta.Modul2.L8
 {
@@ -10,8 +11,13 @@ namespace SzkolaDotNeta.Modul2.L8
     {
         public static void Task8()
         {
-            
-            
+            Console.Write("Wprowadź ciąg znaków: ");
+            string input = Console.ReadLine();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
         }
     }
 }
